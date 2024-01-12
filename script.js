45f1bda5e233bf5656704b00372193599f664b25
Lalan Chaudhary <chaudharylalan28@gmail.com>
2024-01-12 12:30:54 +0530

    12-01-2024

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-gsap.from(".btn",
-{
-    
+gsap.from(".first-line",{
+    y:50,
+    duration:0.5,
+    delay:0.3
 })
+gsap.from(".second-line",{
+    y:75,
+    duration:0.5,
+    delay:0.3
+})
+gsap.from(".third-line",{
+    y:100,
+    duration:0.5,
+    delay:0.3
+})
+
+var ball=document.querySelector(".ball");
+document.addEventListener('mousemove',(e)=>{
+    console.clear();
+    ball.style.cssText="left :"+(e.pageX - 5) +"px; top:"+(e.pageY-5)+"px;";
+    console.log("x="+e.clientX);
+    console.log("y="+e.clientY);
+
+    });
+
