0ce44fa96d56abf38a43192841504510dd3e1e90
Lalan Chaudhary <chaudharylalan28@gmail.com>
2024-01-16 00:29:54 +0530

    jaya ji

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
 gsap.from(".first-line", {
     y: 50,
     duration: 0.5,
-    delay: 0.3
+    delay: 0.3,
+    opacity:0,
 })
 gsap.from(".second-line", {
     y: 75,
     duration: 0.5,
-    delay: 0.3
+    delay: 0.3,
+    opacity:0,
 })
 gsap.from(".third-line", {
     y: 100,
     duration: 0.5,
-    delay: 0.3
+    delay: 0.3,
+    opacity:0,
 })
-
+ 
 var ball = document.querySelector(".ball");
 var icon = document.querySelector(".icon1");
 document.addEventListener('mousemove', (e) => {
-    console.clear();
+    // console.clear();
     ball.style.cssText = "left :" + (e.pageX - 5) + "px; top:" + (e.pageY - 5) + "px;";
     let x = e.clientX;
     let y = e.clientY;
-    console.log("x=" + e.pageX);
-    console.log("y=" + e.pageY);
+
     if (y > 35 && y < 77 && x > 1364 && x < 1402) {
         ball.classList.add("ball2");
         icon.classList.add("icon2");
         ball.style.cssText = "left :" + (e.pageX - 25) + "px; top:" + (e.pageY - 25) + "px;";
-        console.log("okay");
+        // console.log("okay");
     }
     else {
-        // ball.cssText="height :"+(10)+ "px; width :"+(10)+"px;" 
         ball.classList.remove("ball2");
         icon.classList.remove("icon2");
-        console.log("no");
     }
     if (e.pageY > 2140) {
         ball.style.backgroundColor = "white";
@@ -45,4 +45,29 @@ document.addEventListener('mousemove', (e) => {
     }
 
 });
+var index=true;
+let side=document.querySelector(".side-bar");
+document.addEventListener('DOMContentLoaded', function() {
+    // Your code here
+    document.querySelector(".icon1").addEventListener('click', function() {
+        if(index==true)
+        {
+       side.classList.add("side-bar2")
+       index=false;
+        }
+        console.log("okay");
+    });
+});
+document.addEventListener('DOMContentLoaded', function() {
+    // Your code here
+    document.querySelector(".side-icon").addEventListener('click', function() {
+        if(index==false)
+        {
+        side.style.width="0";
+        // document.querySelector(".side-icon").classList.add(".side-icon2");
+        index=true;
+        }
+    });
+});
+
 
